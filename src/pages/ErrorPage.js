c30c7c7949896c172fa70dd48b5f37f8a2eb285e
graphoeil <graphoeil@gmail.com>
2023-08-22 17:00:13 +0200

    working on about page

--- src/pages/ErrorPage.js
+++ src/pages/ErrorPage.js
@@ -5,7 +5,18 @@ import { Link } from 'react-router-dom';
 
 // Component
 const ErrorPage = () => {
-	return <h4>error page</h4>
+
+	// Return
+	return(
+		<Wrapper className="page-100">
+			<section>
+				<h1>404</h1>
+				<h3>Sorry, the page you tried cannot be found...</h3>
+				<Link to="/" className="btn">Back home</Link>
+			</section>
+		</Wrapper>
+	);
+
 };
 
 // Styled
