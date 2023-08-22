c5fdaf2d393d2ff88842e97cb6c9e7bb424b9094
graphoeil <graphoeil@gmail.com>
2023-08-22 17:01:03 +0200

    working on error page

--- src/pages/ErrorPage.js
+++ src/pages/ErrorPage.js
@@ -10,7 +10,7 @@ const ErrorPage = () => {
 	return(
 		<Wrapper className="page-100">
 			<section>
-				<h1>404</h1>
+				<h1>404 !</h1>
 				<h3>Sorry, the page you tried cannot be found...</h3>
 				<Link to="/" className="btn">Back home</Link>
 			</section>
