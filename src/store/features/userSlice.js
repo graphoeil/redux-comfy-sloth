79c89f8e79a574b5011afa95eb7d04ecae142553
graphoeil <graphoeil@gmail.com>
2023-09-11 17:06:06 +0200

    Working on auth0 integration

--- src/store/features/userSlice.js
+++ src/store/features/userSlice.js
@@ -1,28 +1,25 @@
 // Imports
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-import { useAuth0 } from "@auth0/auth0-react";
+import { createSlice } from "@reduxjs/toolkit";
 
 // Initial state
 const initialState = {
-
+	myUser:false
 };
-
-// Async methods
 
 // Slice
 const userSlice = createSlice({
 	name:'user',
 	initialState,
 	reducers:{
-
-	},
-	extraReducers:(builder) => {
-		
+		// Set my user
+		setMyUser:(state, { payload }) => {
+			state.myUser = payload;
+		}
 	}
 });
 
 // Actions export
-export const {  } = userSlice.actions;
+export const { setMyUser } = userSlice.actions;
 
 // Reducer export
 export default userSlice.reducer;
